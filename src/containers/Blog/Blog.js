38c3eae946a3d98ac66ac48dba36e55cf8085417
Can <canx@andrew.cmu.edu>
2019-12-16 16:49:01 -0800

    Add: select post into fullpost

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -3,19 +3,45 @@ import React, { Component } from 'react';
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
+import axios from 'axios';
 import './Blog.css';
 
 class Blog extends Component {
+  state = {
+    posts: [],
+    selectedPostID: null,
+  };
+
+  postSelectedHandler = (selectedPostID) => {
+    this.setState({ selectedPostID: selectedPostID });
+  };
+
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/posts').then((response) => {
+      const posts = response.data.slice(0, 4);
+      posts.forEach((post) => {
+        post['author'] = 'Can Xia';
+      });
+      this.setState({ posts: posts });
+    });
+  }
+
   render() {
+    const posts = this.state.posts.map((post) => {
+      return (
+        <Post
+          key={post.id}
+          title={post.title}
+          author={post.author}
+          clicked={this.postSelectedHandler.bind(this, post.id)}
+        />
+      );
+    });
     return (
       <div>
-        <section className="Posts">
-          <Post />
-          <Post />
-          <Post />
-        </section>
+        <section className="Posts">{posts}</section>
         <section>
-          <FullPost />
+          <FullPost id={this.state.selectedPostID} />
         </section>
         <section>
           <NewPost />
