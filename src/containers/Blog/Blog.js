eeacef53201ed6e488f25da8ad5a5f183d4e4283
Can <canx@andrew.cmu.edu>
2019-12-17 01:13:25 -0800

    Add: nav frame; Modify: folder structure

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 
 import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
+import FullPost from './FullPost/FullPost';
+import NewPost from './NewPost/NewPost';
 import axios from '../../axios';
 import './Blog.css';
 
@@ -47,7 +47,19 @@ class Blog extends Component {
       });
     }
     return (
-      <div>
+      <div className="Blog">
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <a href="/">Home</a>
+              </li>
+              <li>
+                <a href="/">New Post</a>
+              </li>
+            </ul>
+          </nav>
+        </header>
         <section className="Posts">{posts}</section>
         <section>
           <FullPost id={this.state.selectedPostID} />
